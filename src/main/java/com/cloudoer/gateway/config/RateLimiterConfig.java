f5ad800472863337ee9c410eb10df0a359cc7998
liuxiaokun <liuxk@cloudoer.com>
2019-04-27 17:53:48 +0800

    add rate limiter

---
+++ src/main/java/com/cloudoer/gateway/config/RateLimiterConfig.java
@@ -0,0 +1,25 @@
+package com.cloudoer.gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimiterConfig {
+
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+    }
+
+//    @Bean
+//    KeyResolver tokenKeyResolver() {
+//        return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("userId"));
+//    }
+//
+//    @Bean
+//    KeyResolver uriKeyResolver() {
+//        return exchange -> Mono.just(exchange.getRequest().getPath().value());
+//    }
+}
