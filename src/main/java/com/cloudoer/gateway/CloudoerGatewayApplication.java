433b5bc9fade12b5d497e9bc8682eb92ec050b94
liuxiaokun <liuxk@cloudoer.com>
2019-04-27 15:01:49 +0800

    init gateway project

---
+++ src/main/java/com/cloudoer/gateway/CloudoerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cloudoer.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudoerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudoerGatewayApplication.class, args);
+    }
+
+}
