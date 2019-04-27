faf75a0b82e7e8a36d5f8c42d7f41f84da1db181
liuxiaokun <liuxk@cloudoer.com>
2019-04-27 15:17:53 +0800

    add eureka plugin to support routing by service name

--- src/main/java/com/cloudoer/gateway/CloudoerGatewayApplication.java
+++ src/main/java/com/cloudoer/gateway/CloudoerGatewayApplication.java
@@ -2,8 +2,10 @@ package com.cloudoer.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudoerGatewayApplication {
 
     public static void main(String[] args) {
